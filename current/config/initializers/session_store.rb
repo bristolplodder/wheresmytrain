fb201be5df805b151be91caad990fd9e35cd2093
Louise Crow <louise.crow@gmail.com>
2012-10-25 22:18:48 +0100

    Reformat hash parameters - looks like these got a bit mangled.

--- current/config/initializers/session_store.rb
+++ current/config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Wheresmytrain::Application.config.session_store :cookie_store, key: '_wheresmytrain_session'
+Wheresmytrain::Application.config.session_store :cookie_store, :key => '_wheresmytrain_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
